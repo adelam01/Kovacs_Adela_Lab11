50338403702faf114579a5f7e66de355204231c3
Kovacs Adela <adela17.21@icloud.com>
2023-01-09 19:19:13 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kovacs_Adela_Lab11.Models;
+
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Kovacs_Adela_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
